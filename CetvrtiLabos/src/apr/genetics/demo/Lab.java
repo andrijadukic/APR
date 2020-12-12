a51ce4973a806d9dc9fd26f7daeb9d44ec5f1ea0
Andrija Dukic <andrija.dukic@fer.hr>
2020-12-12 10:20:21 +0100

    napravio kostur zadataka

--- CetvrtiLabos/src/apr/genetics/demo/Lab.java
+++ CetvrtiLabos/src/apr/genetics/demo/Lab.java
@@ -1,8 +1,13 @@
 package apr.genetics.demo;
 
 import apr.genetics.algorithms.EliminationGeneticAlgorithm;
+import apr.genetics.algorithms.conditions.StoppingCondition;
+import apr.genetics.algorithms.conditions.StoppingConditions;
 import apr.genetics.operators.crossover.binary.BinarySinglePointCrossover;
+import apr.genetics.operators.crossover.binary.BinaryUniformCrossover;
+import apr.genetics.operators.crossover.floatinpoint.SimpleArithmeticCrossover;
 import apr.genetics.operators.mutation.binary.BinarySimpleMutation;
+import apr.genetics.operators.mutation.floatingpoint.FloatingPointSimpleMutation;
 import apr.util.Interval;
 
 import static apr.genetics.demo.Testing.*;
@@ -12,24 +17,84 @@ public class Lab {
 
     public static void main(String[] args) {
         zadatak1();
+        zadatak2();
+        zadatak3();
+    }
+
+    private static void zadatak3() {
+        int lb = -50;
+        int ub = 150;
+
+        var floatingPointGA = new EliminationGeneticAlgorithm(new SimpleArithmeticCrossover(), 0.8,
+                new FloatingPointSimpleMutation(0.02, lb, ub), 1.,
+                3);
+
+        var binaryGA = new EliminationGeneticAlgorithm(new BinarySinglePointCrossover(), 1.,
+                new BinarySimpleMutation(0.03), 1.,
+                3);
+
+        StoppingCondition stoppingCondition = StoppingConditions.maxIter(100000);
+
+        run(floatingPointGA, 10, floatingPointPopulation(80, new Interval(lb, ub), 3, FitnessFunctions.f6().negate()), stoppingCondition);
+        run(binaryGA, 10, binaryPopulation(80, new Interval(lb, ub), 1e-4, 3, FitnessFunctions.f6().negate()), stoppingCondition);
+
+        run(floatingPointGA, 10, floatingPointPopulation(80, new Interval(lb, ub), 6, FitnessFunctions.f6().negate()), stoppingCondition);
+        run(binaryGA, 10, binaryPopulation(80, new Interval(lb, ub), 1e-4, 6, FitnessFunctions.f6().negate()), stoppingCondition);
+
+        run(floatingPointGA, 10, floatingPointPopulation(80, new Interval(lb, ub), 3, FitnessFunctions.f7().negate()), stoppingCondition);
+        run(binaryGA, 10, binaryPopulation(80, new Interval(lb, ub), 1e-4, 3, FitnessFunctions.f7().negate()), stoppingCondition);
+
+        run(floatingPointGA, 10, floatingPointPopulation(80, new Interval(lb, ub), 6, FitnessFunctions.f7().negate()), stoppingCondition);
+        run(binaryGA, 10, binaryPopulation(80, new Interval(lb, ub), 1e-4, 6, FitnessFunctions.f7().negate()), stoppingCondition);
+    }
+
+    private static void zadatak2() {
+        int lb = -50;
+        int ub = 150;
+
+        var floatingPointGA = new EliminationGeneticAlgorithm(new SimpleArithmeticCrossover(), 0.8,
+                new FloatingPointSimpleMutation(0.02, lb, ub), 1.,
+                3);
+
+        var binaryGA = new EliminationGeneticAlgorithm(new BinarySinglePointCrossover(), 1.,
+                new BinarySimpleMutation(0.03), 1.,
+                3);
+
+        int[] dimensions = new int[]{1, 3, 6, 10};
+        for (int dimension : dimensions) {
+            test(floatingPointGA, floatingPointPopulation(80, new Interval(lb, ub), dimension, FitnessFunctions.f6().negate()));
+            test(binaryGA, binaryPopulation(80, new Interval(lb, ub), 1e-6, dimension, FitnessFunctions.f6().negate()));
+
+            test(floatingPointGA, floatingPointPopulation(80, new Interval(lb, ub), dimension, FitnessFunctions.f7().negate()));
+            test(binaryGA, binaryPopulation(80, new Interval(lb, ub), 1e-6, dimension, FitnessFunctions.f7().negate()));
+        }
     }
 
     private static void zadatak1() {
         int lb = -50;
         int ub = 150;
-        int dimension = 2;
-        var function = FitnessFunctions.f1().negate();
 
-//        var floatingPointGA = new EliminationGeneticAlgorithm(new SimpleArithmeticCrossover(), 0.8,
-//                new FloatingPointSimpleMutation(0.02, lb, ub), 1.,
-//                3);
-//
-//        test(floatingPointGA, floatingPointPopulation(80, new Interval(lb, ub), dimension, function));
+        var floatingPointGA = new EliminationGeneticAlgorithm(new SimpleArithmeticCrossover(), 0.8,
+                new FloatingPointSimpleMutation(0.02, lb, ub), 1.,
+                3);
 
-        var binaryGA = new EliminationGeneticAlgorithm(new BinarySinglePointCrossover(), 0.8,
+        var binaryGA = new EliminationGeneticAlgorithm(new BinarySinglePointCrossover(), 1.,
                 new BinarySimpleMutation(0.03), 1.,
                 3);
 
-        test(binaryGA, binaryPopulation(80, new Interval(lb, ub), 50, dimension, function));
+        test(floatingPointGA, floatingPointPopulation(80, new Interval(lb, ub), 2, FitnessFunctions.f1().negate()));
+        test(binaryGA, binaryPopulation(80, new Interval(lb, ub), 1e-6, 2, FitnessFunctions.f1().negate()));
+
+        test(floatingPointGA, floatingPointPopulation(80, new Interval(lb, ub), 5, FitnessFunctions.f3().negate()));
+        test(binaryGA, binaryPopulation(80, new Interval(lb, ub), 1e-6, 5, FitnessFunctions.f3().negate()));
+
+        test(floatingPointGA, floatingPointPopulation(80, new Interval(lb, ub), 2, FitnessFunctions.f6().negate()));
+        test(binaryGA, binaryPopulation(80, new Interval(lb, ub), 1e-6, 2, FitnessFunctions.f7().negate()));
+
+        test(floatingPointGA, floatingPointPopulation(80, new Interval(lb, ub), 2, FitnessFunctions.f1().negate()));
+        test(binaryGA, binaryPopulation(80, new Interval(lb, ub), 1e-6, 2, FitnessFunctions.f1().negate()));
+
+        test(floatingPointGA, floatingPointPopulation(80, new Interval(lb, ub), 2, FitnessFunctions.f1().negate()));
+        test(binaryGA, binaryPopulation(80, new Interval(lb, ub), 1e-6, 2, FitnessFunctions.f1().negate()));
     }
 }
--- CetvrtiLabos/src/apr/genetics/demo/Testing.java
+++ CetvrtiLabos/src/apr/genetics/demo/Testing.java
@@ -1,15 +1,31 @@
 package apr.genetics.demo;
 
 import apr.genetics.algorithms.GeneticAlgorithm;
+import apr.genetics.algorithms.conditions.StoppingCondition;
 import apr.genetics.algorithms.conditions.StoppingConditions;
 import apr.genetics.algorithms.util.NthGenerationObserver;
 import apr.genetics.algorithms.util.StandardOutputLogger;
+import apr.genetics.chromosomes.Chromosome;
 import apr.genetics.chromosomes.binary.Decoders;
 import apr.genetics.chromosomes.population.ListBasedPopulation;
 import apr.genetics.chromosomes.population.Population;
 import apr.util.Interval;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Testing {
+
+    public static List<Double> run(GeneticAlgorithm geneticAlgorithm, int runs, Population population, StoppingCondition stoppingCondition) {
+        List<Chromosome> fittest = new ArrayList<>(runs);
+        for (int i = 0; i < runs; i++) {
+            fittest.add(geneticAlgorithm.run(population, stoppingCondition).getFittest());
+        }
+        List<Double> fitness = fittest.stream().map(Chromosome::getFitness).collect(Collectors.toList());
+        System.out.println(fitness.stream().map(String::valueOf).collect(Collectors.joining(", ")));
+        return fitness;
+    }
 
     public static Population floatingPointPopulation(int populationSize, Interval interval, int dimension, FitnessFunction fitnessFunction) {
         return new ListBasedPopulation(() -> new MinimizationProblemFloatingPointChromosome(interval, dimension, fitnessFunction), populationSize);
